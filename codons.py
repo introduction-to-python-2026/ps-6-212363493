12a5635ac4bb261e017de41b63b4b8dce937cd70
212363493 <sandra_sh08@outlook.com>
2025-11-27 13:35:35 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,4 +1,11 @@
 def create_codon_dict(file_path):
-    pass # Replace the pass with your code
+  codon_dict = {}
+  for line in lines:
+    line = line.strip()
+    cells = line.split('\t')
+    key = cells [0]
+    value = cells [2]
+    codon_dict.update ({key:value})
+  return codon_dict
 
 
